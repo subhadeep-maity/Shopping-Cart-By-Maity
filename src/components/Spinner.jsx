6b01d4b4466efa6dbbd3960de0acc2a39e958804
Subhadeep Maity <156651977+subhadeep-maity@users.noreply.github.com>
2024-07-31 14:50:48 +0530

    Add files via upload

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Spinner.css"
+
+const Spinner = () => {
+  return (
+    <div className='w-screen h-[calc(100vh-80px)] flex justify-center items-center'>
+      <div class="spinner">
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+</div>
+    </div>
+  )
+}
+
+export default Spinner
